b929dc042b24f1f5db044cd161a62e3412725bd6
jimmybabs10 <jimohhabib20@gmail.com>
2022-03-10 15:19:19 -0800

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Entry
+ *
+ *Description: prints a set of string
+ *
+ *return: always 1 (success)
+ */
+int main(void)
+{
+fprintf(stderr,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+}
