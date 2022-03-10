6685ed053c2b419628b2394c1ef29a993ffbf71f
jimmybabs10 <jimohhabib20@gmail.com>
2022-03-10 09:05:56 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *Displays string using puts function
+ *displays a string using puts function
  */
-main(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
