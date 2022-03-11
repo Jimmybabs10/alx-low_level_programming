0681def54cecc65fac6009408dfe59279f7f3472
jimmybabs10 <jimohhabib20@gmail.com>
2022-03-11 13:55:34 -0800

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n == 0)
 {
-printf("is zero");
+printf("%d\n is zero", n);
 }
 else if (n > 0)
 {
-printf("is positive");
+printf("%d\n is positive", n);
 }
 else
 {
-printf("is negative");
+printf("%d\n is negative", n);
 }
 return (0);
 }
