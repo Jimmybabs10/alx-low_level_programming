301a57732c8cb537667af0a0c62c112d665af48a
jimmybabs10 <jimohhabib20@gmail.com>
2022-03-14 11:03:23 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,15 +11,15 @@ int main(void)
 {
 int num = '0';
 
-while (num <= '8')
+while (num <= '9')
 {
 putchar(num);
+if (num == 9)
+break;
 putchar(',');
 putchar(' ');
 num++;
 }
-putchar('9');
-putchar(',');
 putchar('\n');
 return (0);
 }
