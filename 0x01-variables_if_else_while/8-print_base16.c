fc98b113b85e551fb2d4c1150056f30a9243401b
jimmybabs10 <jimohhabib20@gmail.com>
2022-03-14 11:30:18 -0700

    task 9

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,11 +10,16 @@
 int main(void)
 {
 int hex = '0';
-
-while (hex <= 'f')
+int a = 'a';
+while (hex <= '9')
 {
 putchar(hex);
 hex++;
+}
+while (a <= 'f')
+{
+putchar(a);
+a++;
 }
 putchar('\n');
 return (0);
