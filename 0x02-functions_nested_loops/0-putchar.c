e79bf2b32abafb32132fa14ca8f29b7528439cf7
jimmybabs10 <jimohhabib20@gmail.com>
2022-03-15 06:35:59 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main- prints _putchar
+ *
+ *Return: Always 0
+ *
+ */
+
+int main(void)
+{
+char name[] = "_putchar";
+
+putchar(name);
+putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is contains series of task from training with ALX
