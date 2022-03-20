859bf305e8ed6f1beba0f564e1a36aed51d33ec6
jimmybabs10 <jimohhabib20@gmail.com>
2022-03-20 10:32:18 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+/**
+ *_islower - checks for lowercase
+ *
+ *@c: parameter to pass for letters
+ *
+ *Return: 1 if it's lower and 0 if it's not lowr
+ *
+ */
+
+int _isalpha(int c)
+{
+        if (isalpha(c))
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
+}
