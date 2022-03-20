5c3a6ea222e71a846e96d16b99a31a3fe94932fe
jimmybabs10 <jimohhabib20@gmail.com>
2022-03-20 06:57:11 -0700

    task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10- prints the alphabet 10x
+ *
+ *Return: returns nothing
+ *
+ */
+
+void print_alphabet_x10(void)
+char c = a;
+
+{
+while (c <= 'j')
+{
+while (c <= 'z')
+{
+_putchat(c);
+c++;
+}
+c++;
+}
+}
