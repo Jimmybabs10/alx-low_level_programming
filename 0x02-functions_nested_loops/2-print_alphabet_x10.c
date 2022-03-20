025c50d8451453d87ab4ca9976eb681422a0e13f
jimmybabs10 <jimohhabib20@gmail.com>
2022-03-20 07:11:16 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,27 @@
 #include "main.h"
 
 /**
- *print_alphabet_x10- prints the alphabet 10x
+ *print_alphabet_x10 - prints the alphabet 10x
  *
  *Return: returns nothing
  *
  */
 
 void print_alphabet_x10(void)
-char c = 'a';
+{
+char alph;
+int num;
+alph = 'a';
+num = 0;
 
+while (num <= 9)
 {
-while (c <= 'j')
-{
-while (c <= 'z')
+while (alph <= 'z')
 {
 _putchar(c);
 c++;
 }
 c++;
 }
+_putchar('\n');
 }
