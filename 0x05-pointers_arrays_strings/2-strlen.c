09ef8365711addce674d795b0f5982b2a6383c6f
jimmybabs10 <jimohhabib20@gmail.com>
2022-03-21 07:24:54 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string
+ *
+ *@s: parameter to check for length
+ *
+ *Return: nothing
+ *
+ */
+
+int _strlen(char *s)
+{
+	strlen(s);
+}
