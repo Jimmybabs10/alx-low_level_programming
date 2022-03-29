f27498d9899f1412e4f84d71ad5aeb025b40a73b
jimmybabs10 <jimohhabib20@gmail.com>
2022-03-29 10:54:04 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,6 +12,7 @@ char *_strpbrk(char *s, char *accept)
 	unsigned int i, j;
 
 	for (i = 0; s[i]; i++)
+	{
 		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
@@ -20,6 +21,7 @@ char *_strpbrk(char *s, char *accept)
 	if (accept[j])
 	{
 		return (s + i);
+	}
 	}
 	return (0);
 }
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ *print_diagsums - prints the sum of the two diagonals of a square matrix
+ *@a:
+ *@size:
+ *Return:
